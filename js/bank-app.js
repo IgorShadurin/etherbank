e0dd4f07268c89d6351e6563b0a0744a77f93993
shadurin <igor.shadurin@gmail.com>
2017-10-20 19:10:39 +0300

    web3 checker

--- js/bank-app.js
+++ js/bank-app.js
@@ -1,13 +1,49 @@
 angular.module('bankApp', [])
     .controller('BankController', ['$scope', '$window', '$interval', function ($scope, $window, $interval) {
             $scope.name = "Криптобанк";
+            // contract web3 interface
+            $scope.bankContract = null;
+
+            // check until web3 initialized
+            $scope.web3Checker = $interval(function () {
+                if ($scope.isWeb3()) {
+                    $interval.cancel($scope.web3Checker);
+                    $scope.web3Checker = undefined;
+                }
+            }, 100);
+
             $scope.sendRegistration = function () {
                 // todo implement
             };
+
+            // check and initialize web3
             $scope.isWeb3 = function () {
-                // todo implement
-                return true;
+                if (typeof $window.Web3 !== 'undefined') {
+                    $window.web3 = new Web3($window.web3.currentProvider);
+                    console.log('web3 ok');
+                    $scope.bankContract = $window.web3.eth.contract([/*abi here*/]).at(/*wallet here*/);
+                    console.log($scope.bankContract);
+                    $scope.checkNetwork();
+
+                    return true;
+                } else {
+                    console.log('web3 not ok');
+                    return false;
+                }
             };
+
+            // check is test network
+            $scope.checkNetwork = function () {
+                $window.web3.version.getNetwork(function (error, result) {
+                    if (result !== "4") {
+                        alert('Пожалуйста, подключитесь к тестовой сети Ethereum Rinkeby и обновите страницу');
+                        $scope.isWeb3 = function () {
+                            return false;
+                        };
+                    }
+                });
+            };
+
             $scope.checkIsRegistered = function () {
                 // todo implement
                 return true;
