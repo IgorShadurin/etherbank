615e1b8578b4bf054fce1dfd3875ad83ca3b0923
shadurin <igor.shadurin@gmail.com>
2017-10-20 19:01:43 +0300

    create mortal bank

--- contracts/etherbank.sol
+++ contracts/etherbank.sol
@@ -1,8 +1,32 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.15;
 
 
-contract etherbank {
-    function etherbank(){
+contract mortal {
+    /* Define variable owner of the type address */
+    address owner;
+
+    /* This function is executed at initialization and sets the owner of the contract */
+    function mortal() {owner = msg.sender;}
+
+    /* Function to recover the funds on the contract */
+    function kill() {if (msg.sender == owner) selfdestruct(owner);}
+}
+
+
+contract Etherbank is mortal {
+    struct CreditRequest {
+
+    }
+
+    function Etherbank(){
+
+    }
+
+    function createCreditRequest() payable {
+
+    }
+
+    function returnLoan() payable {
 
     }
 }
