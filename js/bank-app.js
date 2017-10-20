7cead27661a50bea010ff8739a8723a135a1e680
shadurin <igor.shadurin@gmail.com>
2017-10-20 18:48:30 +0300

    added func header for angular

--- js/bank-app.js
+++ js/bank-app.js
@@ -1,1 +1,22 @@
-/*hello*/
+angular.module('bankApp', [])
+    .controller('BankController', ['$scope', '$window', '$interval', function ($scope, $window, $interval) {
+            $scope.name = "Криптобанк";
+            $scope.sendRegistration = function () {
+                // todo implement
+            };
+            $scope.isWeb3 = function () {
+                // todo implement
+                return true;
+            };
+            $scope.checkIsRegistered = function () {
+                // todo implement
+                return true;
+            };
+            $scope.createCreditRequest = function () {
+                // todo implement
+            };
+            $scope.returnLoan = function () {
+                // todo implement
+            };
+        }]
+    );
