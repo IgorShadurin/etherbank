24b5f88935c5a73ee5b169c8af69e42b52ff8cd7
shadurin <igor.shadurin@gmail.com>
2017-10-21 01:15:32 +0300

    goods test

--- basic/views/vk/index.php
+++ basic/views/vk/index.php
@@ -5,12 +5,42 @@
 $this->title = 'Items';
 ?>
 <div class="site-index">
-    <?php echo $_GET['api_result'] ?>
-    <div class="row" style="text-align:center">
-        <div class="col-sm-3">
-            <img src="" alt="">
+    <?php //echo $_GET['api_result'] ?>
+    <div class="row goods-row" style="text-align:center">
+        <div class="col-sm-4">
+            <p><img class="goods" src="<?= \yii\helpers\Url::to('@web/images/goods-1.jpg') ?>" alt=""></p>
+            <p>
+                <button class="btn btn-primary btn-buy">Купить в кредит</button>
+            </p>
         </div>
-        <div class="col-sm-3">Hello2</div>
-        <div class="col-sm-3">Hello3</div>
+        <div class="col-sm-4">
+            <p>
+                <img class="goods" src="<?= \yii\helpers\Url::to('@web/images/goods-2.jpg') ?>" alt="">
+            </p>
+            <p>
+                <button class="btn btn-primary btn-buy">Купить в кредит</button>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <p><img class="goods" src="<?= \yii\helpers\Url::to('@web/images/goods-3.jpg') ?>" alt="">
+            </p>
+            <p>
+                <button class="btn btn-primary btn-buy">Купить в кредит</button>
+            </p>
+        </div>
     </div>
 </div>
+
+<style>
+    .goods {
+        width: 100%;
+        max-width: 350px;
+    }
+</style>
+
+<script>
+    $('.btn-buy').click(function () {
+        // todo check is web3 exists - if not exists block buttons
+        // todo web3 call contract
+    });
+</script>
