7cde6b18610d4673a57e5429af05659443fc9ddf
shadurin <igor.shadurin@gmail.com>
2017-10-21 09:17:12 +0300

    added contract info

--- basic/web/js/main.js
+++ basic/web/js/main.js
@@ -1,4 +1,8 @@
 var checkWeb3Interval = null;
+var contractAbi = [];
+var contractWallet;
+var bankContract;
+
 $('.btn-buy').click(function () {
     // todo web3 call contract
 
@@ -8,15 +12,13 @@ checkWeb3Interval = setInterval(function () {
     if (typeof Web3 !== 'undefined') {
         window.web3 = new Web3(window.web3.currentProvider);
         console.log('web3 ok');
-        //$scope.bankContract = $window.web3.eth.contract(contractAbi).at(contractWallet);
-        //console.log($scope.bankContract);
-        //$scope.checkNetwork();
+        alert('add contract info');
+        bankContract = window.web3.eth.contract(contractAbi).at(contractWallet);
+        console.log(bankContract);
         clearInterval(checkWeb3Interval);
         $('.goods-row').removeClass('hide');
         $('.web3-load').hide();
-        //return true;
     } else {
         console.log('web3 not ok');
-        //return false;
     }
 }, 100);
